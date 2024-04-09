e077b3ac521b5496e5d8941c3f7e4e72ee9bb282
Vader1970 <daniel.wilkey@gmail.com>
2024-04-09 18:19:09 +1200

    App finished

--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,35 @@
+// Importing necessary functions from Redux Toolkit Query
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// Accessing the RapidAPI key from environment variables
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+// Creating an API slice using createApi function
 export const articleApi = createApi({
+  // Defining the name for the reducer path
   reducerPath: "articleApi",
+  // Configuring the base query with fetchBaseQuery
+  baseQuery: fetchBaseQuery({
+    // Setting the base URL for API requests
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    // Adding headers to the requests for authentication
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set("X-RapidAPI-Host", "article-extractor-and-summarizer.p.rapidapi.com");
+
+      return headers;
+    },
+  }),
+  // Defining API endpoints using builder.query
   endpoints: (builder) => ({
     getSummary: builder.query({
-      query: (params) => "test",
+      // Defining the query function for the endpoint
+      // encodeURIComponent() function encodes special characters that may be present in the parameter values
+      // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+      query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
     }),
   }),
 });
+
+// Exporting the generated hooks for making queries
+export const { useLazyGetSummaryQuery } = articleApi;
--- src/services/store.js
+++ src/services/store.js
@@ -1,10 +1,18 @@
+// Importing necessary functions from Redux Toolkit
 import { configureStore } from "@reduxjs/toolkit";
 
+// Importing the API slice for managing articles
 import { articleApi } from "./article";
 
+// Creating the Redux store with configureStore function
 export const store = configureStore({
+  // Configuring reducers for the store
   reducer: {
+    // Using the reducer path from articleApi slice
     [articleApi.reducerPath]: articleApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware),
+  // Configuring middleware for the store
+  middleware: (getDefaultMiddleware) =>
+    // Concatenating default middleware with articleApi middleware
+    getDefaultMiddleware().concat(articleApi.middleware),
 });
